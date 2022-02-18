307fc30cf5485a3f2bef29fca3cae8f9b533e300
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2022-02-18 11:38:42 +0600

    useRef

--- practice/src/Components/Form.js
+++ practice/src/Components/Form.js
@@ -1,36 +1,34 @@
-import React, { useState,useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
 import { BookContext } from "./BookProvider";
 
 export default function Form(props) {
-  const [name, setName] = useState("");
-  const [author, setAuthor] = useState("");
+  // const [name, setName] = useState("");
+  // const [author, setAuthor] = useState("");
   const [data, setData] = useContext(BookContext);
+
+  const nameRef = useRef("");
+  const authorRef = useRef("");
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
+        let name_ = nameRef.current.value;
+        let author_ = authorRef.current.value;
         //props.onAdd(name, author);
-        setData({ books: [...data.books, { Name: name, author: author }] });
-        setName("");
-        setAuthor("");
+        setData({ books: [...data.books, { Name: name_, author: author_ }] });
+        // setName("");
+        // setAuthor("");
+        console.log(nameRef.current.value);
       }}
     >
       <label>
         name
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></input>
+        <input type="text" ref={nameRef}></input>
       </label>
       <br />
       <label>
         Author
-        <input
-          type="text"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        ></input>
+        <input type="text" ref={authorRef}></input>
       </label>
       <br />
       <button type="submit">Register</button>
