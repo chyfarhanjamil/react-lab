814fb4382a4644e228e0bc43733549a3b0dd4c45
Chowdhury Farhan Jamil <56598076+chyfarhanjamil@users.noreply.github.com>
2022-02-18 11:53:47 +0600

    routing

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
 import Booklist from "./Components/Booklist";
 import { BookProvider } from "./Components/BookProvider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./Components/Form";
 
 function App() {
   return (
     <BookProvider>
       <div className="App">
-        <Booklist />
+        {/* <<Booklist />
         {/* <Form/> */}
+        
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Booklist />} />
+            <Route path="/form" element={<Form />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </BookProvider>
   );
